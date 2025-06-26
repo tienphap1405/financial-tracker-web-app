74402dd3d6586f6f706c03c20df8e5117ad9408a
tienphap1405 <tienphap1405@gmail.com>
2025-06-25 21:54:24 -0600

    Update page.js

--- app/monthly-detail-page/page.js
+++ app/monthly-detail-page/page.js
@@ -8,6 +8,15 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+
+    fontWeight: 'bold', // Add this line
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  fontWeight: 'bold',
+}));
+
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
@@ -27,11 +36,11 @@ export default function MonthlyDetailPage() {
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                     <TableRow className='bg-gray-200'>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+                        <StyledTableCell align="right">Calories</StyledTableCell>
+                        <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+                        <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+                        <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
