1a13b79b38fd81e2c623eeea9920122764f39b51
tienphap1405 <tienphap1405@gmail.com>
2025-07-02 19:59:52 -0600

    Update overview-table.js

--- app/components/overview-table.js
+++ app/components/overview-table.js
@@ -26,7 +26,7 @@ export default function Overview() {
     return (
         <div className='flex flex-col pt-10 mx-auto'>
             
-            <div className='grid grid-cols-4 gap-4 mx-10'>
+            <div className='grid grid-cols-3 gap-4 mx-10'>
                 <div className="flex bg-white p-10 rounded-lg shadow-xl flex-col text-black border-1">
                     <div className="flex flex-col items-center p-1">
                         <h1>Income Summary this Month</h1>
@@ -59,46 +59,38 @@ export default function Overview() {
    
 
             <div className="flex bg-white p-6 w-100 rounded-lg shadow-xl mt-10 ml-10 mr-10 flex-col text-black border-1">
-                <div className="flex justify-between">
-                    <h1 className="text-xl font-bold">Monthly View Analytics</h1>
-                    <h1 className="text-lg font-bold">Month: {currentMonthName}/{currentYear}</h1>
-                </div>
-                
-                <hr></hr>
-                
-                <div className="flex justify-between mt-2 ">
-                        <PieChart
-                            series={[
-                                {
-                                    data: [
-                                        { id: 0, value: testdata[currentMonth]?.rent || 0, label: 'Rent' },
-                                        { id: 1, value: testdata[currentMonth]?.utilities || 0, label: 'Utilities & Services bill' },
-                                        { id: 2, value: testdata[currentMonth]?.Others || 0, label: 'Others' },
-                                        { id: 3, value: testdata[currentMonth]?.Transport || 0, label: 'Public Transport' },
-                                    ],
-                                    highlightScope: { fade: 'global', highlight: 'item' },
-                                    faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-                                },
-                            ]}
-                            width={200}
-                            height={200}
-                        />
-
-                        <BarChart
-                            dataset={[testdata[currentMonth]]}
-                            xAxis={[{ dataKey: 'month' }]}
-                            series={[
-                                { dataKey: 'rent', label: 'Rent' },
-                                { dataKey: 'utilities', label: 'Utilities & Services bill' },
-                                { dataKey: 'Others', label: 'Others' },
-                                { dataKey: 'Transport', label: 'Public Transport' },
-                            ]}
-                            {...chartSetting}
-                        />
-                </div>
-              
-
+                    <PieChart
+                        series={[
+                            {
+                                data: [
+                                    { id: 0, value: testdata[currentMonth]?.rent || 0, label: 'Rent' },
+                                    { id: 1, value: testdata[currentMonth]?.utilities || 0, label: 'Utilities & Services bill' },
+                                    { id: 2, value: testdata[currentMonth]?.Others || 0, label: 'Others' },
+                                    { id: 3, value: testdata[currentMonth]?.Transport || 0, label: 'Public Transport' },
+                                ],
+                                highlightScope: { fade: 'global', highlight: 'item' },
+                                faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+                            },
+                        ]}
+                        width={200}
+                        height={200}
+                    />
             </div>
+
+            <div className="flex bg-white p-6 w-100 rounded-lg shadow-xl mt-10 ml-10 mr-10 flex-col text-black border-1">
+                    <BarChart
+                        dataset={[testdata[currentMonth]]}
+                        xAxis={[{ dataKey: 'month' }]}
+                        series={[
+                            { dataKey: 'rent', label: 'Rent' },
+                            { dataKey: 'utilities', label: 'Utilities & Services bill' },
+                            { dataKey: 'Others', label: 'Others' },
+                            { dataKey: 'Transport', label: 'Public Transport' },
+                        ]}
+                        {...chartSetting}
+                    />
+            </div>
+              
         </div>
     );
 }
